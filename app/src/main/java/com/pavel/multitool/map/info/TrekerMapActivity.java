439d10f97ca985ed8971f459ecd7854de61fd47f
”Pavel <cbet0mup.pro@gmail.com>
2020-11-21 14:44:18 +0700

    add tracker and map api

--- app/src/main/java/com/pavel/multitool/map/info/TrekerMapActivity.java
+++ app/src/main/java/com/pavel/multitool/map/info/TrekerMapActivity.java
@@ -2,19 +2,26 @@ package com.pavel.multitool.map.info;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.location.Location;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.pavel.multitool.R;
 
+import java.util.List;
+
 public class TrekerMapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private UiSettings uiSettings;
+    private List<Location> savedLocations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,24 +31,34 @@ public class TrekerMapActivity extends FragmentActivity implements OnMapReadyCal
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        LocationBreedcrumb locationBreedcrumb = (LocationBreedcrumb) getApplicationContext();
+        savedLocations = locationBreedcrumb.getMyLocations();
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        uiSettings = mMap.getUiSettings();
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        uiSettings.setZoomControlsEnabled(true);            // показывать кнопки зума
+
+        LatLng lastLocationPlaced = sydney;
+
+
+        for (Location l : savedLocations) {
+            LatLng latLng = new LatLng(l.getLatitude(), l.getLongitude());
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(latLng);
+            markerOptions.title("Lat: " + l.getLatitude() + "  Lon: " + l.getLongitude());
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+            mMap.addMarker(markerOptions);
+            lastLocationPlaced = latLng;
+        }
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(lastLocationPlaced, 12.0f));
     }
 }
